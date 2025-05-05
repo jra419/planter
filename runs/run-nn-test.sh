454c7ad2278e6e691d141f36442f08cf4406d1d8
jra <joao.amado@tecnico.ulisboa.pt>
2025-05-05 13:01:21 +0000

    Add run scripts for rf and nn.

---
+++ runs/run-nn-test.sh
@@ -0,0 +1,1 @@
+sudo python3 Planter.py -c conf/cic-ids-2018/test-nn.json
---
+++ runs/run-rf-kitsune.sh
@@ -0,0 +1,26 @@
+sudo python3 Planter.py -c conf/kitsune/active-wiretap-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/active-wiretap-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/active-wiretap-rf-2.json
+# sudo python3 Planter.py -c conf/kitsune/active-wiretap-rf-3.json
+sudo python3 Planter.py -c conf/kitsune/arp-mitm-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/arp-mitm-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/arp-mitm-rf-2.json
+# sudo python3 Planter.py -c conf/kitsune/arp-mitm-rf-3.json
+sudo python3 Planter.py -c conf/kitsune/dos-syn-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/dos-syn-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/dos-syn-rf-2.json
+sudo python3 Planter.py -c conf/kitsune/fuzzing-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/fuzzing-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/fuzzing-rf-2.json
+sudo python3 Planter.py -c conf/kitsune/os-scan-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/os-scan-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/os-scan-rf-2.json
+sudo python3 Planter.py -c conf/kitsune/ssdp-flood-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/ssdp-flood-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/ssdp-flood-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/ssl-renegotiation-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/ssl-renegotiation-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/ssl-renegotiation-rf-2.json
+sudo python3 Planter.py -c conf/kitsune/video-injection-rf-0.json
+sudo python3 Planter.py -c conf/kitsune/video-injection-rf-1.json
+sudo python3 Planter.py -c conf/kitsune/video-injection-rf-2.json
