2603f8e2e0fc96bccfda94c50db4c4fd2d4a3aeb
jra <joao.amado@tecnico.ulisboa.pt>
2025-06-11 17:00:51 +0100

    Add if+pca conf files and scripts.

---
+++ runs/run-if-pca-cic-ids-2018.sh
@@ -0,0 +1,17 @@
+cd ..
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/botnet-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/botnet-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ddos-hoic-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ddos-hoic-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ddos-loic-http-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ddos-loic-http-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-goldeneye-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-goldeneye-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-hulk-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-hulk-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-slowhttptest-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-slowhttptest-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-slowloris-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/dos-slowloris-if-0-pca-2.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ssh-bruteforce-if-0-pca-1.json
+sudo python3 Planter.py -c conf/cic-ids-2018/if-pca/ssh-bruteforce-if-0-pca-2.json
---
+++ runs/run-if-pca-kitsune.sh
@@ -0,0 +1,17 @@
+cd ..
+sudo python3 Planter.py -c conf/kitsune/if-pca/active-wiretap-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/active-wiretap-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/arp-mitm-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/arp-mitm-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/dos-syn-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/dos-syn-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/fuzzing-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/fuzzing-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/os-scan-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/os-scan-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/ssdp-flood-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/ssdp-flood-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/ssl-renegotiation-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/ssl-renegotiation-if-0-pca-2.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/video-injection-if-0-pca-1.json
+sudo python3 Planter.py -c conf/kitsune/if-pca/video-injection-if-0-pca-2.json
